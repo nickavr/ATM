eb393d6be7938422a09c69a5c8eed27af54a15b3
ZZ02SR826 <nicolae.avram@ibm.com>
2021-05-10 19:16:14 +0300

    Small Enum setup

--- src/main/java/ro/ibm/bootcamp2021/ATMcore/CurrencyType.java
+++ src/main/java/ro/ibm/bootcamp2021/ATMcore/CurrencyType.java
@@ -3,6 +3,13 @@ package ro.ibm.bootcamp2021.ATMcore;
 public enum CurrencyType {
     RON("RON"), EURO("€"), DOLLAR("$");
 
+    private String currencySign;
+
     CurrencyType(String currency) {
+        this.currencySign = currency;
+    }
+
+    public String getCurrencySign() {
+        return currencySign;
     }
 }
