ded4cc69717187ec3c3543b2636dc7eec61f837d
ZZ02SR826 <nicolae.avram@ibm.com>
2021-05-17 21:52:55 +0300

    started session logic

--- src/main/java/ro/ibm/bootcamp2021/APIs/AuthentificationAPI.java
+++ src/main/java/ro/ibm/bootcamp2021/APIs/AuthentificationAPI.java
@@ -1,5 +1,26 @@
 package ro.ibm.bootcamp2021.APIs;
 
+import com.google.gson.Gson;
+import ro.ibm.bootcamp2021.AccountCore.Account;
+import ro.ibm.bootcamp2021.Exceptions.UnknownCVVCodeException;
+import ro.ibm.bootcamp2021.Exceptions.UnknownPINException;
+
 public class AuthentificationAPI {
-    //TODO: check cvv, check pin
+
+    private static Account account = null;
+
+    public static void beginSession(Integer CVV, Integer PIN){
+        try{
+            account.checkCVVCode(CVV);
+            account.checkPIN(PIN);
+            //TODO: read txt file and transform to json obj array
+        } catch (UnknownCVVCodeException | UnknownPINException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static Account getAccount() {
+        return account;
+    }
 }
--- src/main/java/ro/ibm/bootcamp2021/AccountCore/Account.java
+++ src/main/java/ro/ibm/bootcamp2021/AccountCore/Account.java
@@ -1,4 +1,30 @@
 package ro.ibm.bootcamp2021.AccountCore;
 
-public class Account {
+import ro.ibm.bootcamp2021.Exceptions.UnknownCVVCodeException;
+import ro.ibm.bootcamp2021.Exceptions.UnknownPINException;
+
+public class Account implements AccountActions{
+    String firstName;
+    String lastName;
+    Integer CVV;
+    Integer PIN;
+    Double deposit;
+
+    public Account(String firstName, String lastName, Integer CVV, Integer PIN, Double deposit) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.CVV = CVV;
+        this.PIN = PIN;
+        this.deposit = deposit;
+    }
+
+    @Override
+    public void checkCVVCode(Integer code) throws UnknownCVVCodeException {
+
+    }
+
+    @Override
+    public void checkPIN(Integer pin) throws UnknownPINException {
+
+    }
 }
